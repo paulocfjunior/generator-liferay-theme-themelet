fb532f7e7bd53ed90df3047fc2a36c7b45f4046b
paulocfjunior <paulo.junior@gs.liferay.com>
2018-07-13 10:07:28 -0300

    Updated imports and gitignores in templates

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var liferayThemeTasks = require('liferay-theme-tasks');
+var liferayThemeTasks = require('liferay-theme-tasks-themelet');
 
 liferayThemeTasks.registerTasks({
 	gulp: gulp
